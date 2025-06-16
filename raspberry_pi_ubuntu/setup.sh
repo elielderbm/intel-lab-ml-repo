93284bb113f9aead2d2ba86bfe97dfdba999fa1d
Elielder B. de Melo <elielderbelchior@gmail.com>
2025-06-16 00:09:15 -0300

    fix: rasp: setup: adding parameter to pip install

--- raspberry_pi_ubuntu/setup.sh
+++ raspberry_pi_ubuntu/setup.sh
@@ -7,7 +7,7 @@ sudo apt update
 sudo apt install -y python3 python3-pip
 
 # Install Python dependencies
-pip install -r requirements.txt
+pip install --break-system-packages -r requirements.txt
 
 # Install PyTorch for ARM (CPU)
 pip install torch --index-url https://download.pytorch.org/whl/cpu
